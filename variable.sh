9e95c7705a209d529cbf3e81d74de5468fb1738a
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-20 19:27:28 +0530

    var commit

--- variable.sh
+++ variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#declare var
-person1=ram 
-person2=raja
+#declare varibale and passing through command
+person1=$1
+person2=$2
 
 echo $person1:hi ho are you
 echo $person2:fyn!... how are you
