6e4723762b27627dac4db2fbca0052ed863e7562
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-20 19:38:35 +0530

    comit

---
+++ confidental.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#for confidental info we cant decalare values we can provide read option 
+
+echo "please enter username::"
+
+read please enter username
+
+
+echo "please enter password::"
+
+read please enter username
