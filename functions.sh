0a4587fb8cf7b8afeb15313352e36e11974af4e8
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-21 16:51:45 +0530

    updated

--- functions.sh
+++ functions.sh
@@ -4,21 +4,27 @@ USERID=$[ id -u ]
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP-$USERID.log
+
+### colours varaibales
+R="\e31m"
+G="\e32m"
+Y="\e33m"
+
 if [ $USERID -ne 0 ]
 then
-  echo -e "\e[31m go with sudo access"
+  echo -e   $R"  RUN with sudo access"$N
   exit 1
 else
-  echo -e "\e[32m sudo user"
+  echo -e  $G" RUNNING with sudo user"$N
 fi
 
 VALIDATE(){
 if [ $? -ne 0 ]
 then
-   echo -e "\e[31m script failed"
+   echo -e  $R"script failed"$N
    exit 1
 else
-  echo -e "\e[32m script passed"
+  echo -e $R"script passed"$N
 fi
 }
 
