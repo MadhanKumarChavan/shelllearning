aa0892f646f52eb6e52d136a2db20f756a97d267
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-21 09:36:14 +0530

    comit

---
+++ specialvar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo list of values: $@  #list all values given 
+echo count of values:$#  #count of all values given
+echo current working directory: $PWD
+echo name of script: $0
+echo home dir: $home
+echo user name: $USER
