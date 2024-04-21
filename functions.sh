cb04b0fdbdc9ac9c9d43930af472701d4b455c2e
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-21 16:38:42 +0530

    colours comit

--- functions.sh
+++ functions.sh
@@ -6,19 +6,19 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP-$USERID.log
 if [ $USERID -ne 0 ]
 then
-  echo "go with sudo access"
+  echo -e "\e [31m go with sudo access"
   exit 1
 else
-  echo "sudo user"
+  echo -e "\e [32m sudo user"
 fi
 
 VALIDATE(){
 if [ $? -ne 0 ]
 then
-   echo "script failed"
+   echo -e "\e [31m script failed"
    exit 1
 else
-  echo "script passed"
+  echo -e "\e [32m script passed"
 fi
 }
 
