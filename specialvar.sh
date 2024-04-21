3212387cfdbcfe215bbfb9649cf0d4d2ff440e40
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-21 09:41:52 +0530

    update

--- specialvar.sh
+++ specialvar.sh
@@ -4,5 +4,12 @@ echo list of values: $@  #list all values given
 echo count of values:$#  #count of all values given
 echo current working directory: $PWD
 echo name of script: $0
-echo home dir: $home
+echo home dir: $HOME
 echo user name: $USER
+echo hostname: $hostname
+echo uname : $uname
+echo PID: $$
+
+echo list all :$LS
+
+echo sleep60
