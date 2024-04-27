b0e6c9ca59da757de962c1be77971ed026627ac0
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-27 16:31:31 +0530

    commit

---
+++ disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$( df -hT | grep xfs )
+DISK_THERSHOLD=10
+
+while IFS= read -r line
+do
+  USAGE=$( echo $line | grep xfs | awk -F " " '{ print$6F } ' | cut -d "%" -f1 )
+  FOLDER=$ ( echo $line | awk -F " " '{ print$NF}' )
+  if $USAGE -ge $DISK_THERSHOLD
+  echo "$FOLDER is more than $DISK_THERSHOLD, current usage:$DISK_USAGE"
+
+
+done <<< $DISK_USAGE
